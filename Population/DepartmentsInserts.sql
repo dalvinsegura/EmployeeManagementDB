9e231fe794a20ecac30a4a454ab7a8ba9505f7f4
TheB05 <branderbaez02@gmail.com>
2024-07-31 18:43:28 -0400

    Department data insertions.

---
+++ Population/DepartmentsInserts.sql
@@ -0,0 +1,11 @@
+INSERT INTO departments (name, location) VALUES
+('Recursos Humanos', 'Santo Domingo'),
+('Finanzas', 'Santo Domingo'),
+('Ventas', 'Santiago'),
+('Marketing', 'Santo Domingo'),
+('Tecnología de la Información', 'Santo Domingo'),
+('Operaciones', 'La Romana'),
+('Servicio al Cliente', 'Santiago'),
+('Logística', 'Haina'),
+('Producción', 'San Pedro de Macorís'),
+('Investigación y Desarrollo', 'Santo Domingo');
